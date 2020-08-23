7ccf8c4575245968170f7c29b304f5618c700be2
Alexy Long <alexy.long@orange.fr>
2020-08-23 18:02:51 +0200

    Removed primitives

--- src/components.rs
+++ src/components.rs
@@ -1,8 +1,5 @@
 mod camera;
 mod light;
-mod primitives;
 
 pub use self::camera::*;
 pub use self::light::*;
-pub use self::primitives::*;
-
--- src/components/primitives.rs
+++
@@ -1,7 +0,0 @@
-mod cube;
-mod disk;
-mod plane;
-
-pub use self::cube::*;
-pub use self::disk::*;
-pub use self::plane::*;
